c702bab9459e14b1c81a67d76dc5fca299077f8d
Robert Husák <robert@husak.cloud>
2017-02-14 18:51:42 +0100

    Add simple C# project

---
+++ samples/Basic/Arithmetic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basic
+{
+    public class Arithmetic
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
