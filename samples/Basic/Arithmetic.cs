d055a4f4b77441b966ff6c106f1faa959b441a7c
Robert Husák <robert@husak.cloud>
2017-02-17 15:54:19 +0100

    Enhance provided examples

--- samples/Basic/Arithmetic.cs
+++ samples/Basic/Arithmetic.cs
@@ -4,14 +4,76 @@ namespace Basic
 {
     public class Arithmetic
     {
+        public static int AddSimple()
+        {
+            int a = 1;
+            int b = 10;
+
+            int c = a + b;
+
+            return c;
+        }
+
         public static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public static void AddTest()
+        {
+            int x = 10;
+            int y = 20;
+
+            int w = Add(x, y);
         }
 
         public static int Subtract(int a, int b)
         {
             return a - b;
         }
+
+        public static MyStruct AddStruct(MyStruct a, MyStruct b)
+        {
+            var result = new MyStruct()
+            {
+                X = a.X + b.X,
+                Y = a.Y + b.Y
+            };
+
+            var other = new MyStruct(1, 2);
+
+            return result;
+        }
+
+        public static int PassByRef(int x, ref int y, out int z)
+        {
+            y = -x;
+            z = x + y;
+
+            return y + z;
+        }
+
+        public static void PassByRefTest()
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+
+            int d = PassByRef(a, ref b, out c);
+        }
+    }
+
+    
+    public struct MyStruct
+    {
+        public MyStruct(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
     }
 }
---
+++ samples/Basic/References.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Basic
+{
+    public class Point
+    {
+        public Point(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.z = z;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int z;
+    }
+
+    public class References
+    {
+        public static void InstanceTest()
+        {
+            var point = new Point(1, 2, 3);
+
+            point.X = 2 * point.Y;
+            point.z = point.X + point.z;
+        }
+    }
+}
